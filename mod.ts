ba1ac11a8a6d3d8b539ba97f8231e48e30eef817
Kyle June <github@kylejune.com>
2020-09-13 15:07:46 -0500

    Fix lint

--- mod.ts
+++ mod.ts
@@ -4,14 +4,16 @@ export { Vector } from "./vector.ts";
 export { BinaryHeap } from "./binary_heap.ts";
 export { ascend, descend } from "./comparators.ts";
 export {
-  compare,
-  map,
   swap,
   randomInt,
   RandomIntOptions,
   range,
   RangeOptions,
   shuffle,
+} from "./common.ts";
+export type {
+  compare,
+  map,
 } from "./common.ts";
 export { BSTree } from "./trees/bs_tree.ts";
 export { RBTree } from "./trees/rb_tree.ts";
