ccceae001ef8ae7f8a3ea9fe061802cdafa87724
Kyle June <github@kylejune.com>
2022-03-12 12:19:18 -0600

    Update test dependencies

--- test_deps.ts
+++ test_deps.ts
@@ -3,12 +3,12 @@ export {
   assertEquals,
   assertStrictEquals,
   assertThrows,
-} from "https://deno.land/std@0.120.0/testing/asserts.ts";
+} from "https://deno.land/std@0.129.0/testing/asserts.ts";
 
 export {
   bench,
   runBenchmarks,
-} from "https://deno.land/std@0.120.0/testing/bench.ts";
-export type { BenchmarkTimer } from "https://deno.land/std@0.120.0/testing/bench.ts";
+} from "https://deno.land/std@0.129.0/testing/bench.ts";
+export type { BenchmarkTimer } from "https://deno.land/std@0.129.0/testing/bench.ts";
 
 export { describe, it } from "https://deno.land/x/test_suite@0.12.0/mod.ts";
